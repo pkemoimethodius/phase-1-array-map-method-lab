f6c80e4468e8e08a11b7bf383614e8b8c392c58b
methodius pkemoi <pkemoimethodius@gmail.com>
2024-07-03 18:33:02 +0300

    Adds the case

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
+/*const titleCased = () => {
   return tutorials
+}*/
+const titleCased = () =>{
+  return tutorials.map(tutorial => {
+    return tutorial.split(' ').map(word => {
+      return word[0].toUpperCase() + word.slice(1);
+    }).join(' ');
+  })
 }
